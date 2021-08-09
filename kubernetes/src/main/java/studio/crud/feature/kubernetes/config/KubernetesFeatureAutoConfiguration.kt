d3fd5203651f476834242a1cc39d68a2467d75b6
Idan Elhalwani <idan@elhalwani.com>
2021-08-09 16:25:27 +0300

    Add kubernetes feature

---
+++ kubernetes/src/main/java/studio/crud/feature/kubernetes/config/KubernetesFeatureAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package studio.crud.feature.kubernetes.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("studio.crud.feature.kubernetes")
+class KubernetesFeatureAutoConfiguration
