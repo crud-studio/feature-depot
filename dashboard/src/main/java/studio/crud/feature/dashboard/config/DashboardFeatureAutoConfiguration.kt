45fc529513994539390c59c368c524a266ed7800
Idan Elhalwani <idan@elhalwani.com>
2021-08-16 13:17:27 +0300

    Dashboard feature base

---
+++ dashboard/src/main/java/studio/crud/feature/dashboard/config/DashboardFeatureAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package studio.crud.feature.dashboard.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("studio.crud.feature.dashboard")
+class DashboardFeatureAutoConfiguration
