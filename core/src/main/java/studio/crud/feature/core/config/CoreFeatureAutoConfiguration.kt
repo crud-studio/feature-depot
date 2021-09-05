1f306de63c2fc692edaee8094e2a3208216bad9a
Idan Elhalwani <idan@elhalwani.com>
2021-09-05 17:53:35 +0300

    Add core feature

---
+++ core/src/main/java/studio/crud/feature/core/config/CoreFeatureAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package studio.crud.feature.core.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("studio.crud.feature.core")
+class CoreFeatureAutoConfiguration
