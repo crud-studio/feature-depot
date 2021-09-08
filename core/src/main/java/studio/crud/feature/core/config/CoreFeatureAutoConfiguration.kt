b84cff5df54c560e6b3a3e9c00d4252f87089773
Idan Elhalwani <idan@elhalwani.com>
2021-09-08 22:36:39 +0300

    Add feature depot BOM

--- core/src/main/java/studio/crud/feature/core/config/CoreFeatureAutoConfiguration.kt
+++ core/src/main/java/studio/crud/feature/core/config/CoreFeatureAutoConfiguration.kt
@@ -2,7 +2,10 @@ package studio.crud.feature.core.config
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ComponentScan("studio.crud.feature.core")
 class CoreFeatureAutoConfiguration
