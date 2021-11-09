cec909c1dd690e29baea69ddfac52ba8eec62330
Idan Elhalwani <idan@elhalwani.com>
2021-11-09 11:42:14 +0200

    feat(Auth): add entityUuid to MDC

--- auth/src/main/java/studio/crud/feature/auth/filter/AuthenticationFilter.kt
+++ auth/src/main/java/studio/crud/feature/auth/filter/AuthenticationFilter.kt
@@ -1,10 +1,12 @@
 package studio.crud.feature.auth.filter
 
-import studio.crud.feature.auth.model.TokenAuthenticationRequest
-import studio.crud.feature.auth.trait.RequestAuthenticationTraitResolver
+import org.slf4j.MDC
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import studio.crud.feature.auth.model.TokenAuthentication
+import studio.crud.feature.auth.model.TokenAuthenticationRequest
+import studio.crud.feature.auth.trait.RequestAuthenticationTraitResolver
 import studio.crud.feature.core.web.filter.AbstractExceptionHandlingFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -19,8 +21,10 @@ class AuthenticationFilter(
         val token = request.getToken()
         if(!token.isNullOrBlank()) {
             val traits = requestAuthenticationTraitResolver.resolve(request)
-            val authentication = TokenAuthenticationRequest(token, traits)
-            SecurityContextHolder.getContext().authentication = authenticationManager.authenticate(authentication)
+            val request = TokenAuthenticationRequest(token, traits)
+            val authentication = authenticationManager.authenticate(request) as TokenAuthentication
+            SecurityContextHolder.getContext().authentication = authentication
+            MDC.put("authEntityUuid", authentication.userInfo.entityUuid)
         }
         chain.doFilter(request, response)
     }
