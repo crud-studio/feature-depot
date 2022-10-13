6cb1d5f7b3fc7117c9fbaaf6708ac93ae631e674
Shani Holdengreber <thewizkid@gmail.com>
2022-10-13 10:29:53 +0300

    Fix package export error

--- core/src/main/java/studio/crud/feature/core/crud/transformer/CollectionMappingTransformer.kt
+++ core/src/main/java/studio/crud/feature/core/crud/transformer/CollectionMappingTransformer.kt
@@ -1,21 +1,23 @@
+@file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
+
 package studio.crud.feature.core.crud.transformer
 
+import org.springframework.stereotype.Component
 import studio.crud.crudframework.crud.handler.CrudHandler
 import studio.crud.crudframework.fieldmapper.transformer.base.FieldTransformerBase
-import org.springframework.stereotype.Component
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl
 import java.lang.reflect.Field
 
 @Component("collectionMappingTransformer")
 class CollectionMappingTransformer(
-        private val crudHandler: CrudHandler
+    private val crudHandler: CrudHandler
 ) : FieldTransformerBase<Collection<Any>, Collection<Any>>() {
     override fun innerTransform(fromField: Field, toField: Field, originalValue: Collection<Any>?, fromObject: Any, toObject: Any): Collection<Any>? {
         originalValue ?: return null
         val type = (toField.genericType as ParameterizedTypeImpl).actualTypeArguments[0]
         val baseMapping = originalValue.map { crudHandler.fill(it, type as Class<Any>?) }
 
-        if(Set::class.java.isAssignableFrom(toField.type)) {
+        if (Set::class.java.isAssignableFrom(toField.type)) {
             return baseMapping.toSet()
         }
         return baseMapping
